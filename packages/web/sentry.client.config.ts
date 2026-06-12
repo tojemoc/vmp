f63df2d4f36f24a2cbc419493332c04f10fb0802
Jakub D. <26146502+kubo6472@users.noreply.github.com>
2026-06-12 13:25:09 +0200

    fix sentry with wizzard

--- packages/web/sentry.client.config.ts
+++ packages/web/sentry.client.config.ts
@@ -1,9 +1,32 @@
-import * as Sentry from '@sentry/nuxt'
-import { buildSentryInitOptions } from './utils/sentryOptions'
+import * as Sentry from "@sentry/nuxt";
 
-const config = useRuntimeConfig()
-const initOptions = buildSentryInitOptions(config.public.sentry)
+Sentry.init({
+  // If set up, you can use your runtime config here
+  // dsn: useRuntimeConfig().public.sentry.dsn,
+  dsn: "https://dc7f32b9ba036c0ae8d0346a937448b5@o4511548868198400.ingest.de.sentry.io/4511548881109072",
 
-if (initOptions) {
-  Sentry.init(initOptions)
-}
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+  
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+  
+  // If you don't want to use Session Replay, just remove the line below:
+  integrations: [Sentry.replayIntegration()],
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending of user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nuxt/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
--- packages/web/sentry.server.config.ts
+++ packages/web/sentry.server.config.ts
@@ -1,14 +1,19 @@
-import * as Sentry from '@sentry/nuxt'
-import { buildSentryInitOptions, parseEnvBoolean, parseTracesSampleRate } from './utils/sentryOptions'
+import * as Sentry from "@sentry/nuxt";
+ 
+Sentry.init({
+  dsn: "https://dc7f32b9ba036c0ae8d0346a937448b5@o4511548868198400.ingest.de.sentry.io/4511548881109072",
 
-const sentry = useRuntimeConfig().public.sentry
-const initOptions = buildSentryInitOptions({
-  dsn: sentry.dsn || '',
-  tracesSampleRate: parseTracesSampleRate(String(sentry.tracesSampleRate ?? '')),
-  environment: sentry.environment || '',
-  enableLogs: parseEnvBoolean(String(sentry.enableLogs ?? '')),
-})
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
 
-if (initOptions) {
-  Sentry.init(initOptions)
-}
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending of user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nuxt/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
